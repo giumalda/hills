e4575eb444f3a0203b3f92cc2154cc4a751a1a98
giumalda <maldarizzigiuseppe0@gmail.com>
2026-09-13 21:19:22 +0200

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -18,6 +18,10 @@ import { OrderProvider } from "../components/order/OrderProvider";
 import { SiteNav } from "../components/SiteNav";
 import { WhatsAppFab } from "../components/WhatsAppFab";
 
+const paperTextureStyle = {
+  backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.8' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.04'/%3E%3C/svg%3E")`,
+};
+
 function NotFoundComponent() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background px-4">
@@ -164,11 +168,16 @@ function RootComponent() {
   return (
     <QueryClientProvider client={queryClient}>
       <OrderProvider>
-        <SiteNav />
-        <Outlet />
-        <SiteFooter />
-        <OrderPad />
-        <WhatsAppFab />
+        <div
+          className="min-h-screen bg-sun text-ink selection:bg-primary selection:text-primary-foreground"
+          style={paperTextureStyle}
+        >
+          <SiteNav />
+          <Outlet />
+          <SiteFooter />
+          <OrderPad />
+          <WhatsAppFab />
+        </div>
       </OrderProvider>
     </QueryClientProvider>
   );
